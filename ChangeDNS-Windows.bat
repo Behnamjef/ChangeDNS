ff6e48001a9c689c45862bffb3116c4b163a9f53
BehnamJef <behnamjaffarii@gmail.com>
2023-06-20 19:57:14 +0330

    Add loop, exit, auto network select and administrator privilage

--- ChangeDNS-Windows.bat
+++ ChangeDNS-Windows.bat
@@ -1,26 +1,54 @@
 @echo off
 
-REM List available network interfaces
-echo Available network interfaces:
-wmic nic where "NetConnectionID is not null" get index, NetConnectionID | findstr /r /v "^$" | findstr /r /c:"[0-9]"
-
-REM Prompt the user to choose a network interface by index
-set /p interface_index=Enter the index corresponding to the network interface: 
-
-REM Get the name of the chosen network interface
-for /F "skip=1 tokens=1,2 delims= " %%A in ('wmic nic where "NetConnectionID is not null" get index^, NetConnectionID ^| findstr /r /v "^$" ^| findstr /r /c:"[0-9]"') do (
-    if "%%A" equ "%interface_index%" (
-        set interface_name=%%B
-    )
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+    >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+    >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else (
+    goto gotAdmin
+)
+
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+set params= %*
+echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+"%temp%\getadmin.vbs"
+del "%temp%\getadmin.vbs"
+exit /B
+
+:gotAdmin
+pushd "%CD%"
+CD /D "%~dp0"
+:--------------------------------------
+
+:MainMenu
+REM Retrieve the active network interfaces
+for /F "skip=1 tokens=1,2 delims= " %%A in ('wmic nic where "NetConnectionID is not null" get index^, NetConnectionID ^| findstr /r /v "^$" ^| findstr /r /c:"[0-9]"') do (
+    set "interface_index=%%A"
+    set "interface_name=%%B"
+    REM Use the first active interface found
+    goto :SelectedInterface
+)
+
+:SelectedInterface
 REM Check if the chosen network interface name is set
 if not defined interface_name (
-    echo Invalid index. Exiting.
+    echo No active network interface found. Exiting.
     exit /b 1
     pause
 )
 
+:OptionsMenu
 REM Prompt the user to choose an option
 echo DNS options:
 echo 1. Set Shecan DNS
@@ -28,29 +56,40 @@ echo 2. Set Begzar DNS
 echo 3. Set Electro DNS
 echo 4. Set 403 DNS
 echo 5. Clear DNS
+echo 6. Exit
 
-set /p dns_choice=Enter your choice (1, 2): 
+set /p dns_choice=Enter your choice (1-6): 
 
 REM Set the DNS based on the user's choice
 if "%dns_choice%"=="1" (
     netsh interface ip set dns name="%interface_name%" static 178.22.122.100 primary validate=no
     netsh interface ip add dns name="%interface_name%" 185.51.200.2 index=2 validate=no
+    echo Shecan DNS settings changed successfully.
+    goto :OptionsMenu
 ) else if "%dns_choice%"=="2" (    
     netsh interface ip set dns name="%interface_name%" static 185.55.226.26 primary validate=no
     netsh interface ip add dns name="%interface_name%" 185.55.225.25 index=2 validate=no
+    echo Begzar DNS settings changed successfully.
+    goto :OptionsMenu
 ) else if "%dns_choice%"=="3" (    
     netsh interface ip set dns name="%interface_name%" static 78.157.42.101 primary validate=no
     netsh interface ip add dns name="%interface_name%" 78.157.42.100 index=2 validate=no
+    echo Electro DNS settings changed successfully.
+    goto :OptionsMenu
 ) else if "%dns_choice%"=="4" (    
     netsh interface ip set dns name="%interface_name%" static 10.202.10.202 primary validate=no
     netsh interface ip add dns name="%interface_name%" 10.202.10.102 index=2 validate=no
+    echo 403 DNS settings changed successfully.
+    goto :OptionsMenu
 ) else if "%dns_choice%"=="5" (
     netsh interface ip set dns name="%interface_name%" source=dhcp
+    echo DNS settings cleared successfully.
+    goto :OptionsMenu
+) else if "%dns_choice%"=="6" (
+    echo Exiting.
 ) else (
-    echo Invalid choice. Exiting.
-    exit /b 1
-    pause
+    echo Invalid choice. Please try again.
+    goto :OptionsMenu
 )
 
-echo DNS settings changed successfully.
 pause
